40e15541fda842272bcf3227ec5cff8b7e130c1a
Tiziano <tizianofree36@yahoo.it>
2021-10-04 17:42:24 +0200

    created a dinamic page for the home to show the posts in the database(now working with some fake posts created inside the component) and updated the app.js with BrowserRouter and Switch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+//components
+import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+
+//router
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/home" exact>
+            <Home />
+          </Route>
+          <Route path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Post, toPost } from "../../models/Post";
+import { Link } from "react-router-dom";
+
+export default class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: [
+        {
+          id: 1,
+          title: "Update Services",
+          summary: "Update services are tools you can use to let other people know you've updated your blog",
+          body: '',
+          category: 'wordpress',
+          imgPath: 'images/wordpress-logo.png'
+        },
+        {
+          id: 2,
+          title: "First steps with WordPress",
+          summary: "Let's take a step-by.step tour through your Wordpress site and see how the different functions work and how to make your new site your own",
+          body: '',
+          category: 'wordpress',
+          imgPath: 'images/wordpress-logo.png'
+        },
+        {
+          id: 3,
+          title: "WP API Menus",
+          summary: "Currently, the menu-locations/ route for individual menus will return a tree with full menu hierarchy",
+          body: '',
+          category: 'wordpress',
+          imgPath: 'images/wordpress-logo.png'
+        },
+        {
+          id: 4,
+          title: "Components and props",
+          summary: "Concettualmente i componenti sono come funzioni Javascript",
+          body: '',
+          category: 'react',
+          imgPath: 'images/react-logo.png'
+        },
+        {
+          id: 5,
+          title: "Rendering elements",
+          summary: "Gli elementi react sono immutabili. Una volta creato un elemento non puoi cambiare i suoi figli o attributi",
+          body: '',
+          category: 'react',
+          imgPath: 'images/react-logo.png'
+        },
+        {
+          id: 6,
+          title: "Introduction to JSX",
+          summary: "React non obbliga ad utilizzare JSX, ma la maggior parte delle persone lo trovano utile come aiuto visuale quando lavorano con la UI all'interno del codice JavaScript.",
+          body: '',
+          category: 'react',
+          imgPath: 'images/react-logo.png'
+        }
+      ]
+    }
+  }
+
+  render () {
+    const posts = this.state.posts.map(post => 
+      <div key={ post.id } className="post col-12 col-md-4">
+        <img src={ post.imgPath }  alt="post image"/>
+        <div>
+          <h4>{ post.title }</h4>
+          <p>{ post.summary }</p>
+          <Link className="btn btn-outline-primary" to={ `/posts/${post.id}` }>Read All
+          </Link>
+        </div>
+      </div>
+    )
+
+    return (
+      <div className="container">
+        <div className="row g-2">
+          { posts }
+        </div>
+      </div>
+    )
+  }
+}
