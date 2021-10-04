067d7cb27d81c09cbc41a9e1819a2554a1c7a2ab
Tiziano <tizianofree36@yahoo.it>
2021-10-04 17:41:07 +0200

    created two static pages for the about us and not found components

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="text-warning">
+        <h1>Oops, the page you requested was not found 404 error</h1>
+      </div>
+    )
+  }
+}
